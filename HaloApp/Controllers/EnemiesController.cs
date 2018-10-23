a5ccbde752991274e205664ae75eea59b5d186f7
Yair Rodriguez <yrodriguez@aumenta.mx>
2018-10-23 16:15:13 -0500

    Add project files.

---
+++ HaloApp/Controllers/EnemiesController.cs
@@ -0,0 +1,42 @@
+﻿using HaloApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HaloApp.Controllers
+{
+    public class EnemiesController : Controller
+    {
+        // GET: Enemies
+        public async Task<ActionResult> Index()
+        {
+            var enemies = await RequestEnemiesAsync();
+            return View(enemies);
+        }
+
+        private async Task<List<Enemy>> RequestEnemiesAsync()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.AcceptLanguage.Add(StringWithQualityHeaderValue.Parse("en"));
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "fb4676955bae402fb50b49909808f425");
+
+            var uri = "https://www.haloapi.com/metadata/h5/metadata/enemies?" + queryString;
+
+            var response = await client.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            var jsonResult = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Enemy[]>(jsonResult);
+
+            return result.ToList();
+        }
+    }
+}
---
+++ HaloApp/Models/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HaloApp.Models
+{
+    public class Enemy
+    {
+        public string Faction { get; set; }
+        public string Name { get; set; }
+        public object Description { get; set; }
+        public string LargeIconImageUrl { get; set; }
+        public string SmallIconImageUrl { get; set; }
+        public string Id { get; set; }
+        public string ContentId { get; set; }       
+    }
+}
